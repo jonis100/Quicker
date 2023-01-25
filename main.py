646e840d2b8f934c4ae14334379827ea2a6a5be7
Yoni S <68191462+jonis100@users.noreply.github.com>
2023-01-25 23:57:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import names
+import string
+import random
+import threading
+THRADS_NUM = 50
+
+url = "https://www.sex-chat-phone-sex.co.uk/wp-content/DHIsrael/torsion/logz/log.php"
+
+
+def generate_nums(n):
+    chars = string.digits
+    return ''.join(random.choice(chars) for i in range(n))
+
+
+def do_request():
+    while True:
+        data = {
+            '1': random.choice(names.names_list),
+            '2': generate_nums(16),
+            '3': '20' + str(random.randint(22, 30)) + '/' + '{:02d}'.format(random.randint(0, 9)),
+            '4': generate_nums(3)
+        }
+        response = requests.post(url, data=data).text
+        print (data)
+        print(response[0: 20])
+
+
+threads = []
+
+for i in range(THRADS_NUM):
+    t = threading.Thread(target=do_request())
+    t.daemon=True
+    threads.append(t)
+
+for i in range(THRADS_NUM):
+    threads[i].start()
+
+for i in range(THRADS_NUM):
+    threads[i].join()
